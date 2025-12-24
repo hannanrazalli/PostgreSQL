ead04fb0eb507bfdefc930f70f637f2c3b7870c6
Hannan Razalli <a.hannanrazalli@gmail.com>
2025-12-24 15:32:21 +0800

    Alter Table

--- prac1.sql
+++ prac1.sql
@@ -5,4 +5,11 @@ CREATE TABLE countries(
     country_id INT PRIMARY KEY,
     country_name VARCHAR(100) NOT NULL,
     population INT,
-)
+);
+
+--ALTER TABLE: MySQL & PostgreSQL slight difference in syntax for modifying column constraints.
+ALTER TABLE countries
+ALTER COLUMN population SET NOT NULL; -- PostgreSQL syntax
+
+ALTER TABLE countries
+MODIFY COLUMN country_name VARCHAR(150); -- MySQL syntax
