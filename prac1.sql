22de10999c1083752bcafdc0a022eb1918cff245
Hannan Razalli <a.hannanrazalli@gmail.com>
2025-12-27 10:56:33 +0800

    CONCAT

--- prac1.sql
+++ prac1.sql
@@ -91,3 +91,11 @@ FROM qty
 WHERE month IN (4,5)
 GROUP by month;
 
+SELECT
+	CONCAT(ROUND(SUM(transaction_qty * unit_price)/1000,1), "K") AS total_sales,
+    CONCAT(round(SUM(transaction_qty)/1000,1), "k") AS total_quantity_sold,
+    CONCAT(ROUND(COUNT(transaction_qty)/1000,1), "k") AS total_orders
+FROM
+	css
+WHERE
+	transaction_date = '2023-05-18';
